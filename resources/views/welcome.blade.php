60273c8be4df6d5097c3f5ef4bad7e310e9371bf
Mac <youngdero3376@gmail.com>
2025-08-26 20:09:12 +0100

    update

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,246 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Webze - ICO & Crypto Landing Page Template</title>
+    <meta name="description" content="Webze -  ICO & Crypto Landing Page Template">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/animate.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/fontawesome-all.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/swiper-bundle.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/default-icons.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/default.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/aos.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/main.css')}}">
+</head>
+
+<body>
+
+    <!--Preloader-->
+    <div id="preloader">
+        <div class="loader"></div>
+    </div>
+    <!--Preloader-end -->
+
+    <!-- Scroll-top -->
+    <button class="scroll__top scroll-to-target" data-target="html">
+        <i class="fas fa-arrow-up"></i>
+    </button>
+    <!-- Scroll-top-end-->
+
+
+
+
+    <!-- main-area -->
+    <main class="main-area fix">
+
+        <!-- banner-area -->
+        <section class="banner__area banner__bg" data-background="assets/img/banner/hero_bg.svg">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-xl-7 col-lg-8 col-md-10">
+                        <div class="banner__content">
+                            <span class="sub-title wow fadeInUp" data-wow-delay=".2s" data-wow-duration="1s">The Miracle You’ve Been Waiting For Is Already Here</span>
+                            <h2 class="title wow fadeInUp" data-wow-delay=".4s" data-wow-duration="1s">Learn to Build  <span>Passive</span>  Income & Real Wealth Today</h2>
+                            <p class="wow fadeInUp" data-wow-delay=".6s" data-wow-duration="1s">Join Africa’s #1 Millionaire Mentor, Victor Emelotaobi, and discover how to earn from digital asset arbitrage while building your wealth community.</p>
+                            
+                            <video width="520" height="440" controls>
+                                <source src="assets/vid/Leads.mp4" type="video/mp4">
+                                Your browser does not support the video tag.
+                            </video>
+
+                            <span class="banner__content-bottom wow fadeInUp" data-wow-delay=".8s" data-wow-duration="1s">100% FREE for the first 700<a href="#!">members!</a></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="banner__shape">
+                <img src="assets/img/banner/hero_img01.png" alt="shape" class="alltuchtopdown">
+                <img src="assets/img/banner/hero_img02.png" alt="shape" class="rotateme">
+                <img src="assets/img/banner/hero_img03.png" alt="shape" class="alltuchtopdown">
+                <img src="assets/img/banner/hero_bg_shape.svg" alt="shape" class="banner__bg-shape">
+            </div>
+        </section>
+        <!-- banner-area-end -->
+
+        <!-- features-area -->
+        <section id="features" class="features__area section-pt-120">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-6">
+                        <div class="section__title text-center mb-80">
+                            <span class="sub-title">Join the Digital Asset Club Free Now</span>
+                            <h2 class="title">Why Join? </h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="row gutter-y-40">
+                    <div class="col-lg-12">
+                        <div class="features__item">
+                            <div class="features__icon">
+                                <img src="assets/img/icon/features_icon01.png" alt="icon">
+                            </div>
+                            <div class="features__content">
+                                <p style="font-color: #ffffff;">Learn the exact wealth secrets that separate the rich from the poor.
+
+                                Build passive income streams from digital assets.
+
+                                Access mentorship & guidance from a proven Millionaire Coach.
+
+
+                                Be part of a fast-growing wealth community across Africa and beyond.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- <div class="col-lg-6">
+                        <div class="features__item">
+                            <div class="features__icon">
+                                <img src="assets/img/icon/features_icon02.png" alt="icon">
+                            </div>
+                            <div class="features__content">
+                                <h4 class="title">Crypto <span>exchange</span></h4>
+                                <p>A built-in explorer to track transactions</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="features__item features__item-two">
+                            <div class="features__icon">
+                                <img src="assets/img/icon/features_icon03.png" alt="icon">
+                            </div>
+                            <div class="features__content">
+                                <h4 class="title">Real-time <span>data</span></h4>
+                                <p>Global reach with content available in multiple</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="features__item features__item-two">
+                            <div class="features__icon">
+                                <img src="assets/img/icon/features_icon04.png" alt="icon">
+                            </div>
+                            <div class="features__content">
+                                <h4 class="title">Advanced <span>trading</span></h4>
+                                <p>Visual dashboards for trade performance</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="features__item features__item-two">
+                            <div class="features__icon">
+                                <img src="assets/img/icon/features_icon05.png" alt="icon">
+                            </div>
+                            <div class="features__content">
+                                <h4 class="title">Blockchain <span>compliance</span></h4>
+                                <p>Exportable reports for tax and accounting purposes</p>
+                            </div>
+                        </div>
+                    </div> --}}
+                </div>
+            </div>
+            {{-- <div class="features__shape">
+                <img src="assets/img/images/features_shape.png" alt="shape">
+            </div> --}}
+        </section>
+        <!-- features-area-end -->
+
+        <!-- section-divider -->
+        <div class="section-divider">
+            <div class="container">
+                <span></span>
+            </div>
+        </div>
+        <!-- section-divider-end -->
+
+        <!-- work-area -->
+        <section id="work" class="work__area section-py-120">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-6">
+                        <div class="section__title text-center mb-80">
+                            <span class="sub-title">Meet Victor Emelotaobi</span>
+                            {{-- <h5 class="title">Financial Fitness Coach, Millionaire Mentor & Spiritual Leader with over 18+ years of value transforming entrepreneurs around the world. Expert in sales, growth strategies & financial freedom. Passionate about building real, sustainable wealth through mentorship & leverage.</h5> --}}
+                        </div>
+                    </div>
+                </div>
+                <div class="work__item-wrap">
+                    <div class="work__img">
+                        <img src="assets/img/images/brand7.jpeg" alt="img" class="alltuchtopdown">
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-6" data-aos="fade-right" data-aos-delay="0">
+                            <div class="work__item">
+                                <h1 class="number">01</h1>
+                                <h4 class="title">Financial Fitness Coach <span>Millionaire Mentor</span></h4>
+                                <p>Spiritual Leader with over 18+ years of value transforming entrepreneurs around the world. Expert in sales, growth strategies & financial freedom.</p>
+                            </div>
+                          
+                        </div>
+                        <div class="col-lg-6" data-aos="fade-left" data-aos-delay="0">
+                            <div class="work__item work__item-right">
+                                <h1 class="number">02</h1>
+                                <h4 class="title">Passionate about  <span>building real</span></h4>
+                                <p> sustainable wealth through mentorship & leverage.</p>
+                            </div>
+                          
+                        </div>
+                    </div>
+                </div>
+            </div>
+     
+        </section>
+        <!-- work-area-end -->
+
+           <section class="team__area section-py-120">
+            <div class="container">
+                <div class="row">
+                     <form action="#" class="banner__form wow fadeInUp" data-wow-delay=".8s" data-wow-duration="1s">
+                        <div class="col-lg-6" ></div>
+                        {{-- <label for="email"><img src="assets/img/icon/envelope.svg" alt=""></label>
+                        <input type="email" id="email" placeholder="Business email">
+
+                        <label for="email"><img src="assets/img/icon/envelope.svg" alt=""></label>
+                        <input type="email" id="email" placeholder="Business email"> --}}
+
+                        <label for="email"><img src="assets/img/icon/envelope.svg" alt=""></label>
+                        <input type="email" id="email" placeholder="email">
+
+                        <button type="submit" class="tg-btn">get early access</button>
+                    </form>
+                 
+                </div>
+            </div>
+            <div class="team__shape">
+                <img src="assets/img/images/features_shape.png" alt="shape">
+            </div>
+        </section>
+
+  
+
+    </main>
+    <!-- main-area-end -->
+
+    <!-- JS here -->
+    <script src="{{asset('assets/js/vendor/jquery-3.6.0.min.js')}}"></script>
+    <script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('assets/js/swiper-bundle.min.js')}}"></script>
+    <script src="{{asset('assets/js/jquery.marquee.min.js')}}"></script>
+    <script src="{{asset('assets/js/ajax-form.js')}}"></script>
+    <script src="{{asset('assets/js/jquery.countdown.min.js')}}"></script>
+    <script src="{{asset('assets/js/jquery.easing.js')}}"></script>
+    <script src="{{asset('assets/js/wow.min.js')}}"></script>
+    <script src="{{asset('assets/js/aos.js')}}"></script>
+    <script src="{{asset('assets/js/main.js')}}"></script>
+</body>
+
+</html>
