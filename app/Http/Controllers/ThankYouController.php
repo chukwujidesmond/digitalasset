c7d0cf0871b12cbb8869626621a484eabd36d977
Mac <youngdero3376@gmail.com>
2025-08-27 16:03:41 +0100

    update

--- app/Http/Controllers/ThankYouController.php
+++ app/Http/Controllers/ThankYouController.php
@@ -13,7 +13,9 @@ class ThankYouController extends Controller
    public function sendFeedback(Request $request)
    {
         $body = $request->all();
-
+         if(!$request->email){
+            return redirect()->away('https://chat.whatsapp.com/CnRl6vTjpLO2xXr1hdwpCX?mode=ems_copy_c');
+         }
         Mail::to($request->email)->send(new ThankyouMail($body));
         Session::put('successMessage', 'Account created. Thanks');
        return redirect()->away('https://chat.whatsapp.com/CnRl6vTjpLO2xXr1hdwpCX?mode=ems_copy_c');
