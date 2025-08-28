34ef1e9e1b3363685eaa32611b8cf07c98100789
Mac <youngdero3376@gmail.com>
2025-08-28 12:02:45 +0100

    update

--- resources/views/emails/thank_you.blade.php
+++ resources/views/emails/thank_you.blade.php
@@ -1,23 +1,30 @@
 <html>
 <head></head>
-    <body>
-        {{-- <!-- <p>Hey {{ $user->first_name }},</p> --> --}}
-        <p>Welcome to Digital Asset.</p>
-        <h4>Kindly Join Our:</h4>
-        <ol>
-            <li>COMMUNITY URL: <a href="https://chat.whatsapp.com/Irbu89brcnSHYbxuyzCYhM?mode=ems_copy_t" target='_BLANK'>Join Our community</a></li>
-            {{-- <li>Email: {{ $user->email }}</li>
-            <li>Password: {{ $user->plain_password }}></li> --}}
-        </ol>
-        <h4>Your details:</h4>
-        <ol>
-            <li>Fullname: {{ $body['fullname'] }}</li>
-            <li>Email: {{ $body['email'] }}</li>
-            <li>Phone: {{ $body['phone'] }}</li>
-            <li>Location: {{ $body['location'] }}</li>
-        </ol>
-        {{-- <!-- <p> Need Assistance ? Kindly  <a href='https://support.codova.ai/'><b>Visit</b></a> to checkout our knowledgebase Articles and Chat with our Support Agents.</p>        --> --}}
-        <p>Best Regards,</p>
-        {{-- <!-- <p>Kelechi Mmonu<br>On Behalf of the ¿ Team</p> --> --}}
-    </body>
+<body>
+    <p>Hello Champion,</p>
+
+    <p>Welcome to the world of Digital Asset Investment! You’re now part of our WhatsApp community, where you can ask questions and get direct answers from our admins.</p>
+
+    <h4>Step 1:</h4>
+    <p>Watch the video link attached to this email. It explains everything you need to know to get started.</p>
+    <p><a href="https://youtu.be/Xov_BGnj-68?si=reumt75pooJghAP8" target='_BLANK'>Watch Video</a></p>
+
+    <h4>Step 2:</h4>
+    <p>After watching, return to WhatsApp to:</p>
+    <ul>
+        <li>Ask any questions you have.</li>
+        <li>Schedule a short clarity session with an admin to start earning and building your community.</li>
+    </ul>
+
+     {{-- <ol>
+        <li>Fullname: {{ $body['fullname'] }}</li>
+        <li>Email: {{ $body['email'] }}</li>
+        <li>Phone: {{ $body['phone'] }}</li>
+        <li>Location: {{ $body['location'] }}</li>
+    </ol> --}}
+
+    <p>We’re excited to have you here. Let’s start building your digital wealth today!</p>
+
+    <p>Victor Emelataobi<br>(Financial Fitness Coach | Founder, Leverage Millionaire Digital Asset Club)</p>
+</body>
 </html>
